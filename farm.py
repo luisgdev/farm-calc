9775bd6dcd6718fbd5e991cfa7df8f85fd1a8635
Luis Ch <g298luis@gmail.com>
2021-06-24 20:21:47 -0400

    Simple interest GUI updated

--- farm.py
+++ farm.py
@@ -19,11 +19,9 @@ def simple_interest(cap, apr):
         {'period': 'Daily', 'interest': apr/365 },
         {'period': 'Hour ', 'interest': (apr/365)/24 }
     ]
-    views.header("Interes Simple")
     for item in periods:
-        income = round(cap * item["interest"], 4)
-        interest = round(item["interest"] * 100, 4)
-        print(f'{item["period"]}: ${income} ({interest}%)')
+        item.update({'income': cap * item["interest"]})
+    views.simple_interest(periods)
 
 
 def compound(apr, cap, gas, days):
@@ -52,9 +50,9 @@ if __name__ == '__main__':
     command = ''
     while command.lower() != 'x':
         views.header("Calculadora para Staking pool")
-        cap = float(input('Capital  ($): '))
-        apr = float(input('APR      (%): '))
-        interest = input('Tipo   (S/C): ').lower()
+        cap = float(input('Cap....($): '))
+        apr = float(input('APR....(%): '))
+        interest = input('Tipo.(s/c): ').lower()
         if interest == 'c':
             days = int(input('Staking days: '))
             gas = float(input('Gas fee  ($): '))
--- views.py
+++ views.py
@@ -1,11 +1,30 @@
+from rich.markdown import Markdown
+from rich.console import Console
+from rich.table import Table
+from rich import box
+
+
+def simple_interest(periods):
+    table = Table(title='Interes Simple', box=box.SIMPLE)
+    header = ['Period', 'Income($)', 'Profit(%)']
+    for item in header:
+        table.add_column(item)
+    for item in periods:
+        income = round(item["income"], 4)
+        interest = round(item["interest"] * 100, 4)
+        #print(f'{item["period"]}: ${income} ({interest}%)')
+        table.add_row(item["period"], str(income), str(interest))
+    Console().print(table)
+
+
 def header(title):
-    separator()
-    print(f'*** {title} ***')
-    separator()
+    md = Markdown(f'# {title}')
+    Console().print(md)
 
 
 def separator():
-    print("--" * 18)
+    md = Markdown('___')
+    Console().print(md)
 
 
 def comp_item(item):
